283ca4f3c1fe8ef4c697a123d3fdf4b096d38f52
Daniel Kroening <kroening@cs.ox.ac.uk>
2012-06-27 10:38:51 +0000

    clean

--- src/ebmc/smvlang/smv_language.d
+++
@@ -1,8 +0,0 @@
-smv_language.o: smv_language.cpp smv_language.h ../util/language.h \
-  smv_parse_tree.h ../util/hash_cont.h ../util/string_hash.h \
-  ../util/expr.h ../util/type.h ../util/location.h ../util/irep.h \
-  ../util/dstring.h ../util/string_container.h ../util/hash_cont.h \
-  ../util/string_hash.h ../util/irep_ids.h smv_typecheck.h \
-  ../util/context.h ../util/symbol.h ../util/message.h ../util/location.h \
-  smv_parser.h ../util/parser.h ../util/expr.h ../util/message.h \
-  expr2smv.h
--- src/ebmc/smvlang/smv_parser.d
+++
@@ -1,6 +0,0 @@
-smv_parser.o: smv_parser.cpp smv_parser.h ../util/parser.h ../util/expr.h \
-  ../util/type.h ../util/location.h ../util/irep.h ../util/dstring.h \
-  ../util/string_container.h ../util/hash_cont.h ../util/string_hash.h \
-  ../util/irep_ids.h ../util/message.h ../util/location.h \
-  smv_parse_tree.h ../util/hash_cont.h ../util/string_hash.h \
-  ../util/expr.h
