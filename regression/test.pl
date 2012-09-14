3d1d73cdbd0648df2abbc8ac01952dab9c374364
Daniel Kroening <kroening@cs.ox.ac.uk>
2012-09-14 12:27:15 +0000

    new test script

--- regression/test.pl
+++ regression/test.pl
@@ -8,12 +8,12 @@ use warnings;
 #
 # runs a test and check its output
 
-sub run($$$) {
-  my ($input, $options, $output) = @_;
-  my $cmd = "hw-cbmc $options $input >$output 2>&1";
+sub run($$$$) {
+  my ($input, $cmd, $options, $output) = @_;
+  my $cmdline = "$cmd $options $input >$output 2>&1";
 
-  print LOG "Running $cmd\n";
-  system $cmd;
+  print LOG "Running $cmdline\n";
+  system $cmdline;
   my $exit_value = $? >> 8;
   my $signal_num = $? & 127;
   my $dumped_core = $? & 128;
@@ -41,19 +41,20 @@ sub load($) {
   my ($fname) = @_;
 
   open FILE, "<$fname";
-  my @data = <FILE>;
+  my @data = grep { !/^\/\// } <FILE>;
   close FILE;
 
   chomp @data;
   return @data;
 }
 
-sub test($$) {
-  my ($name, $test) = @_;
-  my ($input, $options, @results) = load($test);
+sub test($$$$$) {
+  my ($name, $test, $t_level, $cmd, $ign) = @_;
+  my ($level, $input, $options, @results) = load($test);
+  $options =~ s/$ign//g if(defined($ign));
 
   my $output = $input;
-  $output =~ s/\.c$/.out/;
+  $output =~ s/\.(c|i|cpp|ii|xml)$/.out/;
 
   if($output eq $input) {
     print("Error in test file -- $test\n");
@@ -61,6 +62,7 @@ sub test($$) {
   }
 
   print LOG "Test '$name'\n";
+  print LOG "  Level: $level\n";
   print LOG "  Input: $input\n";
   print LOG "  Output: $output\n";
   print LOG "  Options: $options\n";
@@ -69,28 +71,46 @@ sub test($$) {
     print LOG "    $result\n";
   }
 
-  my $failed = run($input, $options, $output);
+  if($level eq "CORE") {
+    $level = 1;
+  } elsif($level eq "THOROUGH") {
+    $level = 2;
+  } elsif($level eq "FUTURE") {
+    $level = 4;
+  } elsif($level eq "KNOWNBUG") {
+    $level = 8;
+  } else {
+    die "Level must be one of CORE, THOROUGH, FUTURE or KNOWNBUG\n";
+  }
 
-  if(!$failed) {
-    print LOG "Execution [OK]\n";
-    my $included = 1;
-    foreach my $result (@results) {
-      if($result eq "--") {
-	$included = !$included;
-      } else {
-	my $r;
-	system "grep '$result' '$output' >/dev/null";
-	$r = ($included ? $? != 0 : $? == 0);
-	if($r) {
-	  print LOG "$result [FAILED]\n";
-	  $failed = 1;
-	} else {
-	  print LOG "$result [OK]\n";
-	}
+  my $failed = -1;
+  if($level & $t_level) {
+    $failed = run($input, $cmd, $options, $output);
+
+    if(!$failed) {
+      print LOG "Execution [OK]\n";
+      my $included = 1;
+      foreach my $result (@results) {
+        last if($included == -1);
+        if($result eq "--") {
+          $included--;
+        } else {
+          my $r;
+          system "grep '$result' '$output' >/dev/null";
+          $r = ($included ? $? != 0 : $? == 0);
+          if($r) {
+            print LOG "$result [FAILED]\n";
+            $failed = 1;
+          } else {
+            print LOG "$result [OK]\n";
+          }
+        }
       }
+    } else {
+      print LOG "Execution [FAILED]\n";
     }
   } else {
-    print LOG "Execution [FAILED]\n";
+    print LOG "Execution [SKIPPED]\n";
   }
 
   print LOG "\n";
@@ -102,7 +122,7 @@ sub dirs() {
   my @list;
 
   opendir CWD, ".";
-  @list = grep { !/^\./ && -d "$_" && !/CVS/ } readdir CWD;
+  @list = grep { !/^\./ && -d "$_" && !/CVS/ && -s "$_/test.desc" } readdir CWD;
   closedir CWD;
 
   @list = sort @list;
@@ -110,52 +130,112 @@ sub dirs() {
   return @list;
 }
 
-if(@ARGV != 0) {
-  print "Usage:\n";
-  print "  test.pl\n";
+sub main::VERSION_MESSAGE($$$$) {
+  my ($fh, $getopt, $vers, $opts) = @_;
+  print {$fh} << "EOF";
+test.pl version $vers -- run a series of regression tests
+EOF
+}
+
+sub main::HELP_MESSAGE($$$$) {
+  my ($fh, $getopt, $vers, $opts) = @_;
+  print {$fh} << "EOF";
+
+Usage: test.pl -c CMD [OPTIONS] [DIRECTORIES ...]
+  where OPTIONS are one or more options as listed below; one or more directories
+  may be passed as arguments to test only those. Otherwise all directories in
+  the current location will be used.
+
+  -c CMD     run tests on CMD - required option
+  -i <regex> options in test.desc matching the specified perl regex are ignored
+  -h         show this help and exit
+  -C         core: run all essential tests (default if none of C/T/F/K are given)
+  -T         thorough: run expensive tests
+  -F         future: run checks for future features
+  -K         known: run tests associated with known bugs
+
+
+test.pl expects a test.desc file in each subdirectory. The file test.desc
+follows the format specified below. Any line starting with // will be ignored.
+
+<level>
+<main source>
+<options>
+<required patterns>
+--
+<disallowed patterns>
+--
+<comment text>
+
+where
+  <level>                is one of CORE, THOROUGH, FUTURE or KNOWNBUG
+  <main source>          is a file with extension .c/.i/.cpp/.ii/.xml
+  <options>              additional options to be passed to CMD
+  <required patterns>    one or more lines of regualar expressions that must occur in the output
+  <disallowed patterns>  one or more lines of expressions that must not occur in output
+  <comment text>         free form text
+
+EOF
   exit 1;
 }
+
+use Getopt::Std;
+$main::VERSION = 0.1;
+$Getopt::Std::STANDARD_HELP_VERSION = 1;
+our ($opt_c, $opt_i, $opt_h, $opt_C, $opt_T, $opt_F, $opt_K); # the variables for getopt
+getopts('c:i:hCTFK') or &main::HELP_MESSAGE(\*STDOUT, "", $main::VERSION, "");
+$opt_c or &main::HELP_MESSAGE(\*STDOUT, "", $main::VERSION, "");
+$opt_h and &main::HELP_MESSAGE(\*STDOUT, "", $main::VERSION, "");
+my $t_level = 0;
+$t_level += 2 if($opt_T);
+$t_level += 4 if($opt_F);
+$t_level += 8 if($opt_K);
+$t_level += 1 if($opt_C || 0 == $t_level);
+
+
 
 open LOG,">tests.log";
 
 print "Loading\n";
-my @tests = dirs();
+my @tests = @ARGV != 0 ? @ARGV : dirs();
 my $count = @tests;
-if($count == 1) {
-  print "  $count test found\n";
-} else {
-  print "  $count tests found\n";
-}
-print "\n";
+print "  $count " . (1==$count?"test":"tests") . " found\n\n";
+
+use Cwd qw(getcwd);
 my $failures = 0;
+my $skips = 0;
 print "Running tests\n";
 foreach my $test (@tests) {
   print "  Running $test";
 
+  my $cwd = getcwd;
   chdir $test;
-  my $failed = test($test, "test.desc");
-  chdir "..";
+  my $failed_skipped = test($test, "test.desc", $t_level, $opt_c, $opt_i);
+  chdir $cwd;
 
-  if($failed) {
+  if($failed_skipped < 0) {
+    $skips++;
+    print "  [SKIPPED]\n";
+  } elsif(0 == $failed_skipped) {
+    print "  [OK]\n";
+  } else {
     $failures++;
     print "  [FAILED]\n";
-  } else {
-    print "  [OK]\n";
   }
 }
 print "\n";
 
 if($failures == 0) {
-  print "All tests were successful\n";
+  print "All tests were successful";
 } else {
   print "Tests failed\n";
-  if($failures == 1) {
-    print "  $failures of $count test failed\n";
-  } else {
-    print "  $failures of $count tests failed\n";
-  }
+  print "  $failures of $count " . (1==$count?"test":"tests") . " failed";
 }
+print ", $skips " . (1==$skips?"test":"tests") . " skipped" if($skips > 0);
+print "\n";
+
 
 close LOG;
 
 exit $failures;
+
