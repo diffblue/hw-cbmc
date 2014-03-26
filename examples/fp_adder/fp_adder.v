a16c401ec56d65271de1d17b99340dadfa70a69c
Daniel Kroening <kroening@cs.ox.ac.uk>
2014-03-26 14:28:11 +0000

    more Verilog

--- examples/fp_adder/fp_adder.v
+++ examples/fp_adder/fp_adder.v
@@ -9,9 +9,9 @@
 // Normalises a denormal number or deal with catastrophic cancelation
 
 module normaliseUp(
-  input [9:0] uf_exponent_in,
+  input signed [9:0] uf_exponent_in,
   input [23:0] uf_significand_in,
-  output reg [9:0] uf_exponent,
+  output reg signed [9:0] uf_exponent,
   output reg [23:0] uf_significand);
 
   reg [5:0] shift;
@@ -57,7 +57,7 @@ endmodule
 module unpack(
   input [31:0] f,
   output reg uf_nan, uf_inf, uf_zero, uf_subnormal, uf_sign,
-  output reg [9:0] uf_exponent,
+  output reg signed [9:0] uf_exponent,
   output reg [23:0] uf_significand);
 
   wire sign;
@@ -67,8 +67,8 @@ module unpack(
   // split up f
   assign { sign, exponent, significand } = f;      
   
-  wire [9:0] normaliseUp_exponent;
-  wire [9:0] normaliseUp_significand;
+  wire signed [9:0] normaliseUp_exponent;
+  wire [23:0] normaliseUp_significand;
 
   // this is used for the cae that the number is subnormal
   normaliseUp unpack_normaliseUp(
@@ -86,9 +86,7 @@ module unpack(
         uf_subnormal = 0;
         uf_exponent = 0;
         uf_significand = 0;
-        end
-
-      else begin
+      end else begin
         // subnormal
         uf_nan = 0;
         uf_inf = 0;
@@ -97,10 +95,8 @@ module unpack(
 
         uf_exponent = -126;
         uf_significand = normaliseUp_significand;
-        end
       end
-
-    else if (exponent == 'hff) begin
+    end else if (exponent == 'hff) begin
       if (significand == 0) begin
         // make infinity
         uf_nan = 0;
@@ -109,8 +105,7 @@ module unpack(
         uf_subnormal = 0; 
         uf_exponent = 'hff;
         uf_significand = 0;
-        end
-      else begin
+      end else begin
         // make NaN
         uf_nan = 1;
         uf_inf = 0;
@@ -118,10 +113,8 @@ module unpack(
         uf_subnormal = 0;
         uf_exponent = 'hff;
         uf_significand = 0;
-        end
       end
-
-    else begin
+    end else begin
       // Normal
       uf_nan = 0;
       uf_inf = 0;
@@ -130,13 +123,13 @@ module unpack(
 
       uf_exponent = exponent - `BIAS;
       uf_significand = 'h800000 | significand;
-    end
+    end // if
   end // always
 endmodule
 
 module pack(
   input uf_nan, uf_inf, uf_zero, uf_subnormal, uf_sign,
-  input [9:0] uf_exponent,
+  input signed [9:0] uf_exponent,
   input [23:0] uf_significand,
   output [31:0] f);
   
@@ -152,21 +145,17 @@ module pack(
     if (uf_nan) begin
       exponent = 'hff;
       significand = 'h40b2bd;  // qNaN
-      end
-    else if (uf_inf) begin
+    end else if (uf_inf) begin
       exponent = 'hff;
       significand = 'h0;
-      end
-    else if (uf_zero) begin
+    end else if (uf_zero) begin
       exponent = 'h0;
       significand = 'h0;
-      end
-    else if (uf_subnormal) begin
+    end else if (uf_subnormal) begin
       biased = uf_exponent+`BIAS-1;
       exponent = 'h0;
       significand = uf_significand >> -biased;
-      end
-    else begin
+    end else begin
       exponent = uf_exponent + `BIAS;
       significand = 'h7fffff & uf_significand; // Remove leading 1
     end
@@ -216,8 +205,7 @@ module rounder(
       result_subnormal = 0;
       result_exponent = 0;
       result_significand = 0;
-      end
-    else begin
+    end else begin
       if (result_exponent < -126) begin
         // For subnormals, correct the guard and sticky bits
 
@@ -262,35 +250,77 @@ module rounder(
       case (roundingMode)
         `RNE:
           if (result_exponent > 127) begin
-            //makeInf(result);
+            // make infinity
+            result_nan = 0;
+            result_inf = 1;
+            result_zero = 0;
+            result_subnormal = 0;
+            result_exponent = 'hff;
+            result_significand = 0;            
           end
 
         `RNA:
           if (result_exponent > 127) begin
-            //makeInf(result);
+            // make infinity
+            result_nan = 0;
+            result_inf = 1;
+            result_zero = 0;
+            result_subnormal = 0;
+            result_exponent = 'hff;
+            result_significand = 0;            
           end
 
         `RTP:
           if (result_exponent > 127) begin
             if (result_sign == 0) begin
-              //makeInf(result);
+              // make infinity
+              result_nan = 0;
+              result_inf = 1;
+              result_zero = 0;
+              result_subnormal = 0;
+              result_exponent = 'hff;
+              result_significand = 0;            
             end else begin
-              //makeMax(result);
+              // make max
+              result_nan = 0;
+              result_inf = 0;
+              result_zero = 0;
+              result_subnormal = 0;
+              result_exponent = 127;
+              result_significand = 'hffffff;
             end
           end
         
         `RTN:
           if (result_exponent > 127) begin
             if (result_sign == 1) begin
-              //makeInf(result);
+              // make infinity
+              result_nan = 0;
+              result_inf = 1;
+              result_zero = 0;
+              result_subnormal = 0;
+              result_exponent = 'hff;
+              result_significand = 0;            
             end else begin
-              // makeMax(result);
+              // make max
+              result_nan = 0;
+              result_inf = 0;
+              result_zero = 0;
+              result_subnormal = 0;
+              result_exponent = 127;
+              result_significand = 'hffffff;
             end
           end
 
         `RTZ:
           if (result_exponent > 127) begin
-            //makeMax(result);
+            // make max
+            result_nan = 0;
+            result_inf = 0;
+            result_zero = 0;
+            result_subnormal = 0;
+            result_exponent = 127;
+            result_significand = 'hffffff;
           end
       endcase
 
@@ -316,184 +346,197 @@ module dualPathAdder(
   output reg [9:0] result_exponent,
   output reg [23:0] result_significand);
 
-  `ifdef 0
-  unpackedFloat larger;
-  unpackedFloat smaller;
-  uint8_t guardBit = 0;
-  uint8_t stickyBit = 0;
+  reg guardBit;
+  reg stickyBit;
 
-  // Flags -- result will be normal unless otherwise marked
-  result_nan = 0;
-  result_inf = 0;
-  result_zero = 0;
-  result_subnormal = 0;
+  reg [9:0] larger_exponent, smaller_exponent;
+  reg [23:0] larger_significand, smaller_significant;
+  reg signed [10:0] exponentDifference;
+  reg effectiveSubtract;
+  
+  always @(*) begin
 
-  initUnpackedFloat(&larger);
-  initUnpackedFloat(&smaller);
+    guardBit = 0;
+    stickyBit = 0;
 
-  /* Compute the difference between exponents */
-  int exponentDifference = uf->exponent - ug->exponent;
+    // Flags -- result will be normal unless otherwise marked
+    result_nan = 0;
+    result_inf = 0;
+    result_zero = 0;
+    result_subnormal = 0;
 
-  /* Order by exponent */
-  if ((exponentDifference > 0) || 
-      ((exponentDifference == 0) && (uf->significand > ug->significand))) {
-    larger = *uf;
-    smaller = *ug;
-    result->sign = larger.sign;
+    // Compute the difference between exponents
+    exponentDifference = uf_exponent - ug_exponent;
 
-  } else {
-    larger = *ug;
-    smaller = *uf;
-    exponentDifference = -exponentDifference;
-    result->sign = isAdd ? larger.sign : ~larger.sign;
-
-  }
-
-  result_exponent = larger_exponent;
-
-  // Work out if it is an effective subtract
-  int effectiveSubtract = larger_sign ^ (isAdd ? 0 : 1) ^ smaller_sign;
-
-  // 'decimal point' after the 26th bit, LSB 2 bits are 0
-  // 26 so that we can cancel one and still have 24 + guard bits
-  uint32_t lsig = larger_significand << 2;
-  uint32_t ssig = smaller_significand << 2;
-  uint32_t sum = 'hffffffff;
-
-  // Split the two paths
-  if (exponentDifference <= 1) begin
-
-    /* Near path */
-
-    // Align
-    ssig >>= exponentDifference;
-
-
-    if (effectiveSubtract) {
-
-      /* May have catestrophic cancelation */
-
-      uint32_t diff = lsig - ssig;
-
-      assert((diff & 0xFC000000) == 0);  // result is up to 26 bit
-
-      if (diff == 0) {  // Fully cancelled
-
-	makeZero(result);
-
-	/* IEEE-754 2008 says:
-	 * When the sum of two operands with opposite signs (or the
-	 * difference of two operands with like signs) is exactly
-	 * zero, the sign of that sum (or difference) shall be +0 in
-	 * all rounding-direction attributes except
-	 * roundTowardNegative; under that attribute, the sign of an
-	 * exact zero sum (or difference) shall be −0. However, x + x
-	 * = x − (−x) retains the same sign as x even when x is zero.
-	 */
-	result->sign = (roundingMode == RTN) ? 1 : 0;
-	return; // No need to round
-
-      } else if (diff & 0x02000000) { // 26 bit result
-
-	sum = diff << 1;
-
-	// Sticky bits are 0
-	assert((sum & 0x3) == 0);
-
-	goto extract;
-
-      } else { // Some cancelation
-
-	--result->exponent;
-	result->significand = diff >> 1;
-	guardBit = 0;
-	stickyBit = 0;
-
-	normaliseUp(result);
-
-	// Won't underflow due to an exciting property of subnormal
-	// numbers.  Also, clearly, won't overflow.  Furthermore,
-	// won't increment.  Thus don't need to call the rounder -- as
-	// long as the subnormal flag is correctly set.
-	
-	//	goto rounder;
-
-	result->subnormal = (result->exponent < -126) ? 1 : 0;
-	return;
-      }
+    `ifdef 0
+    /* Order by exponent */
+    if ((exponentDifference > 0) || 
+        ((exponentDifference == 0) && (uf->significand > ug->significand))) {
+      larger = *uf;
+      smaller = *ug;
+      result->sign = larger.sign;
 
     } else {
+      larger = *ug;
+      smaller = *uf;
+      exponentDifference = -exponentDifference;
+      result->sign = isAdd ? larger.sign : ~larger.sign;
 
-      // Near addition is the same as far addition
-      // except without the need to align first.
-      // Thus fall through...
     }
+    `endif
 
-  } else {
+    result_exponent = larger_exponent;
 
+    // Work out if it is an effective subtract
+    effectiveSubtract = larger_sign ^ (isAdd ? 0 : 1) ^ smaller_sign;
 
-    /* Far path */
-    if (exponentDifference > 26) {
-      
-      result->significand = larger.significand;
-      result->subnormal = larger.subnormal;
-      return;
-      
-    } else {
-      
-      if (effectiveSubtract) {
-	ssig = (~ssig) + 1;
-      }
-      
+    `ifdef 0
+    // 'decimal point' after the 26th bit, LSB 2 bits are 0
+    // 26 so that we can cancel one and still have 24 + guard bits
+    uint32_t lsig = larger_significand << 2;
+    uint32_t ssig = smaller_significand << 2;
+    uint32_t sum = 'hffffffff;
+    `endif
+
+    // Split the two paths
+    if (exponentDifference <= 1) begin
+
+      /* Near path */
+
+      `ifdef 0
       // Align
-      int i;
+      ssig >>= exponentDifference;
 
-      for (i = 1; i <= 26; i <<= 1) {
-	if (exponentDifference & i) {
-	  uint32_t iOnes = ((1<<i) - 1);
-	  stickyBit |= ((ssig & iOnes) ? 1 : 0);
-	  
-	  // Sign extending shift
-	  if (effectiveSubtract) {
-	    ssig = (ssig >> i) | (iOnes << (32 - i));
-	  } else {
-	    ssig = (ssig >> i);
-	  }
-	  
-	}
+
+      if (effectiveSubtract) {
+
+        /* May have catestrophic cancelation */
+
+        uint32_t diff = lsig - ssig;
+
+        assert((diff & 0xFC000000) == 0);  // result is up to 26 bit
+
+        if (diff == 0) {  // Fully cancelled
+
+          makeZero(result);
+
+          /* IEEE-754 2008 says:
+           * When the sum of two operands with opposite signs (or the
+           * difference of two operands with like signs) is exactly
+           * zero, the sign of that sum (or difference) shall be +0 in
+           * all rounding-direction attributes except
+           * roundTowardNegative; under that attribute, the sign of an
+           * exact zero sum (or difference) shall be −0. However, x + x
+           * = x − (−x) retains the same sign as x even when x is zero.
+           */
+          result->sign = (roundingMode == RTN) ? 1 : 0;
+          return; // No need to round
+
+        } else if (diff & 0x02000000) { // 26 bit result
+
+          sum = diff << 1;
+
+          // Sticky bits are 0
+          assert((sum & 0x3) == 0);
+
+          goto extract;
+
+        } else { // Some cancelation
+
+          --result->exponent;
+          result->significand = diff >> 1;
+          guardBit = 0;
+          stickyBit = 0;
+
+          normaliseUp(result);
+
+          // Won't underflow due to an exciting property of subnormal
+          // numbers.  Also, clearly, won't overflow.  Furthermore,
+          // won't increment.  Thus don't need to call the rounder -- as
+          // long as the subnormal flag is correctly set.
+          
+          //	goto rounder;
+
+          result->subnormal = (result->exponent < -126) ? 1 : 0;
+          return;
+        }
+
+      } else {
+
+        // Near addition is the same as far addition
+        // except without the need to align first.
+        // Thus fall through...
       }
-    }
-  }
-  
-  // Decimal point is after 26th bit
-  sum = lsig + ssig;
-  
-  if (effectiveSubtract) begin
-    if (sum & 0x02000000ul) { // 26 bit
-      sum <<= 1;
-    } else {                  // 25 bit
-      --result_exponent;
-      sum <<= 2;
-    }
-    // Decimal point is now after the 27th bit
+      `endif
+
+    end else begin
+
+      `ifdef 0
+      /* Far path */
+      if (exponentDifference > 26) {
+        
+        result->significand = larger.significand;
+        result->subnormal = larger.subnormal;
+        return;
+        
+      } else {
+        
+        if (effectiveSubtract) {
+          ssig = (~ssig) + 1;
+        }
+        
+        // Align
+        int i;
+
+        for (i = 1; i <= 26; i <<= 1) {
+          if (exponentDifference & i) {
+            uint32_t iOnes = ((1<<i) - 1);
+            stickyBit |= ((ssig & iOnes) ? 1 : 0);
+            
+            // Sign extending shift
+            if (effectiveSubtract) {
+              ssig = (ssig >> i) | (iOnes << (32 - i));
+            } else {
+              ssig = (ssig >> i);
+            }
+            
+          }
+        }
+      `endif
     end
-  else begin
-    if (sum & 0x04000000ul)
-      ++result_exponent;
-    else
-      sum <<= 1;
-  end
-  
- extract:
-  // Decimal point is now after the 27th bit
-  result_significand = sum >> 3;
-  guardBit = (sum >> 2) & 0x1;
-  stickyBit |= (((sum >> 1) & 0x1) | (sum & 0x1));
-  
-  // Can be simplified as the subnormal case implies no rounding
- rounder:
-  rounder(roundingMode, result, guardBit, stickyBit);
-  `endif
+
+    `ifdef 0    
+    // Decimal point is after 26th bit
+    sum = lsig + ssig;
+    
+    if (effectiveSubtract) begin
+      if (sum & 0x02000000ul) { // 26 bit
+        sum <<= 1;
+      } else {                  // 25 bit
+        --result_exponent;
+        sum <<= 2;
+      }
+      // Decimal point is now after the 27th bit
+      end
+    else begin
+      if (sum & 0x04000000ul)
+        ++result_exponent;
+      else
+        sum <<= 1;
+    end
+    
+   extract:
+    // Decimal point is now after the 27th bit
+    result_significand = sum >> 3;
+    guardBit = (sum >> 2) & 0x1;
+    stickyBit |= (((sum >> 1) & 0x1) | (sum & 0x1));
+    
+    // Can be simplified as the subnormal case implies no rounding
+   rounder:
+    rounder(roundingMode, result, guardBit, stickyBit);
+    `endif
+    
+  end // always
 
 endmodule // dualPathAdder
 
@@ -507,7 +550,7 @@ module fp_add_sub(
   // unpack f
 
   wire uf_nan, uf_inf, uf_zero, uf_subnormal, uf_sign;
-  wire [9:0] uf_exponent;
+  wire signed [9:0] uf_exponent;
   wire [23:0] uf_significand;
 
   unpack unpack_f(f, uf_nan, uf_inf, uf_zero, uf_subnormal, uf_sign, uf_exponent, uf_significand);
@@ -515,20 +558,32 @@ module fp_add_sub(
   // unpack g
 
   wire ug_nan, ug_inf, ug_zero, ug_subnormal, ug_sign;
-  wire [9:0] ug_exponent;
+  wire signed [9:0] ug_exponent;
   wire [23:0] ug_significand;
   
   unpack unpack_g(g, ug_nan, ug_inf, ug_zero, ug_subnormal, ug_sign, ug_exponent, ug_significand);
   
+  // instantiate the dual-path adder
+
+  wire dp_nan, dp_inf, dp_zero, dp_subnormal, dp_sign;
+  wire signed [9:0] dp_exponent;
+  wire [23:0] dp_significand;  
+
+  dualPathAdder dualPathadder(
+    isAdd, roundingMode,
+    uf_nan, uf_inf, uf_zero, uf_subnormal, uf_sign, uf_exponent, uf_significand,
+    ug_nan, ug_inf, ug_zero, ug_subnormal, ug_sign, ug_exponent, ug_significand,
+    dp_nan, dp_inf, dp_zero, dp_subnormal, dp_sign, dp_exponent, dp_significand);
+
   // set up unpacked result
 
   reg result_nan, result_inf, result_zero, result_subnormal, result_sign;
-  reg [9:0] result_exponent;
+  reg signed [9:0] result_exponent;
   reg [23:0] result_significand;
   
   // internal variable
   reg flip;
-
+  
   always @(*) begin
 
     // Handle all the special cases
@@ -541,9 +596,7 @@ module fp_add_sub(
       result_subnormal = 0;
       result_exponent = 'hff;
       result_significand = 0;
-      end
-
-    else if (uf_inf) begin
+    end else if (uf_inf) begin
       if (ug_inf && 
           ((isAdd) ? uf_sign != ug_sign : uf_sign == ug_sign)) begin
         // make NaN
@@ -553,8 +606,7 @@ module fp_add_sub(
         result_subnormal = 0;
         result_exponent = 'hff;
         result_significand = 0;
-        end
-      else begin
+      end else begin
         // make infinity
         result_nan = 0;
         result_inf = 1;
@@ -563,10 +615,8 @@ module fp_add_sub(
         result_exponent = 'hff;
         result_significand = 0;                    
         result_sign = uf_sign;
-        end
       end
-
-    else if (ug_inf) begin
+    end else if (ug_inf) begin
       // make infinity
       result_nan = 0;
       result_inf = 1;
@@ -575,9 +625,7 @@ module fp_add_sub(
       result_exponent = 'hff;
       result_significand = 0;                    
       result_sign = (isAdd) ? ug_sign : !ug_sign;
-      end
-
-    else if (uf_zero) begin
+    end else if (uf_zero) begin
       if (ug_zero) begin
         // make zero
         result_nan = 0;
@@ -594,9 +642,7 @@ module fp_add_sub(
         else 
           result_sign = uf_sign | (flip ^ ug_sign);
 
-        end
-      
-      else begin // !ug_zero
+      end else begin // !ug_zero
         // copy ug
         result_nan=ug_nan;
         result_inf=ug_inf;
@@ -608,10 +654,8 @@ module fp_add_sub(
         
         // adjust sign
         result_sign = isAdd ? result_sign : !result_sign;
-        end
       end
-
-    else if (ug_zero) begin
+    end else if (ug_zero) begin
       // copy uf
       result_nan=uf_nan;
       result_inf=uf_inf;
@@ -620,10 +664,15 @@ module fp_add_sub(
       result_sign=uf_sign;
       result_exponent=uf_exponent;
       result_significand=uf_significand;
-      end
-
-    else begin
-      //dualPathAdder(isAdd,roundingMode,uf,ug,result);
+    end else begin
+      // use output from dual-path adder
+      result_nan=dp_nan;
+      result_inf=dp_inf;
+      result_zero=dp_zero;
+      result_subnormal=dp_subnormal;
+      result_sign=dp_sign;
+      result_exponent=dp_exponent;
+      result_significand=dp_significand;
     end
 
   end // always
