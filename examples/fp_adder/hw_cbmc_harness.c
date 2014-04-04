6086ae8c2d9f989e807a7e894a112d099f242d46
Daniel Kroening <kroening@cs.ox.ac.uk>
2014-04-04 18:02:05 +0000

    fix harness conversion

--- examples/fp_adder/hw_cbmc_harness.c
+++ examples/fp_adder/hw_cbmc_harness.c
@@ -15,18 +15,20 @@ int compareFloat (float f, float g);
 
 int main()
 {
-  float C_result, f, g;
+  float f, g;
+  float C_result, Verilog_result;
   
   // sync the inputs
-  fp_add_sub.f=f;
-  fp_add_sub.g=g;
+  fp_add_sub.f=*(unsigned *)&f;
+  fp_add_sub.g=*(unsigned *)&g;
   fp_add_sub.isAdd=1;
   fp_add_sub.roundingMode=0;
   
   set_inputs();
+  Verilog_result=*(float *)&fp_add_sub.result;
   
   C_result=add(0, f, g);
   
   // check the output
-  assert(compareFloat(C_result, fp_add_sub.result));
+  assert(compareFloat(C_result, Verilog_result));
 }
