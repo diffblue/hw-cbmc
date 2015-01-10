cf2925048c1fae05c9aa09bfd25e070936f522bf
Daniel Kroening <kroening@cs.ox.ac.uk>
2015-01-10 22:47:21 +0000

    missing iostream header

--- src/ebmc/output_verilog.cpp
+++ src/ebmc/output_verilog.cpp
@@ -7,6 +7,7 @@ Author: Daniel Kroening, kroening@kroening.com
 \*******************************************************************/
 
 #include <cstdlib>
+#include <iostream>
 
 #include <util/i2string.h>
 #include <util/arith_tools.h>
