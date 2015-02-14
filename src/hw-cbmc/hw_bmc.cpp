00478c5c8d57918ac460b491056bfcb40a516345
Daniel Kroening <kroening@cs.ox.ac.uk>
2015-02-14 12:20:36 +0000

    more status printing

--- src/hw-cbmc/hw_bmc.cpp
+++ src/hw-cbmc/hw_bmc.cpp
@@ -34,7 +34,9 @@ void hw_bmct::do_unwind_module(
   
   const symbolt &symbol=ns.lookup(unwind_module);
 
-  status() << "Unwinding transition system " << symbol.name << eom;
+  status() << "Unwinding transition system " << symbol.name
+           << " with " << unwind_no_timeframes
+           << " time frames" << eom;
 
   ::unwind(
     to_trans_expr(symbol.value),
@@ -43,6 +45,9 @@ void hw_bmct::do_unwind_module(
     unwind_no_timeframes,
     ns,
     true);
+
+  status() << "Unwinding transition system done" << eom;
+
 }
 
 /*******************************************************************\
