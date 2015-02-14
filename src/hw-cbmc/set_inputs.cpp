a463afa73e4186722f22bc444f9ce260bcf1c846
Daniel Kroening <kroening@cs.ox.ac.uk>
2015-02-14 12:26:46 +0000

    more typing

--- src/hw-cbmc/set_inputs.cpp
+++ src/hw-cbmc/set_inputs.cpp
@@ -35,8 +35,6 @@ void add_set_inputs(
   const std::set<irep_idt> &top_level_inputs,
   const namespacet &ns)
 {
-  code_blockt block;
-
   // the variables we use
   const symbolt &struct_symbol=ns.lookup(struct_identifier);
   const symbolt &array_symbol=ns.lookup(id2string(struct_identifier)+"_array");
@@ -47,22 +45,27 @@ void add_set_inputs(
   
   // We now assume current input values to be equal,
   // with the goal of adding assumptions on inputs.
-  exprt index_expr(ID_index, struct_symbol.type);
-  index_expr.copy_to_operands(symbol_expr(array_symbol), symbol_expr(timeframe_symbol));
+  const index_exprt index_expr(
+    symbol_expr(array_symbol),
+    symbol_expr(timeframe_symbol),
+    struct_symbol.type);
+
+  code_blockt block;
 
   for(std::set<irep_idt>::const_iterator
       i_it=top_level_inputs.begin();
       i_it!=top_level_inputs.end();
       i_it++)
   {
-    struct_typet::componentt component=struct_type.get_component(*i_it);
+    const struct_typet::componentt component=
+      struct_type.get_component(*i_it);
     assert(component.is_not_nil());
   
-    exprt member_expr1=member_exprt(symbol_expr(struct_symbol), *i_it, component.type());
-    exprt member_expr2=member_exprt(index_expr, *i_it, component.type());
+    const exprt member_expr1=member_exprt(symbol_expr(struct_symbol), *i_it, component.type());
+    const exprt member_expr2=member_exprt(index_expr, *i_it, component.type());
   
-    code_assumet input_assumption(equal_exprt(member_expr1, member_expr2));
-    block.move_to_operands(input_assumption);
+    const code_assumet input_assumption(equal_exprt(member_expr1, member_expr2));
+    block.add(input_assumption);
   }
 
   // add code to symbol
