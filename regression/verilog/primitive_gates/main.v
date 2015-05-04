62dba2e6b7274df1228b4b1bed3913305f75d4e5
Daniel Kroening <kroening@cs.ox.ac.uk>
2015-05-04 13:10:13 +0000

    binary buf

--- regression/verilog/primitive_gates/main.v
+++ regression/verilog/primitive_gates/main.v
@@ -5,13 +5,14 @@ module main(and_in1, and_in2, or_in1, or_in2, buf_in, not_in);
   input [31:0] or_in1, or_in2;
   wire  [31:0] or_out;
   input [31:0] buf_in;
-  wire  [31:0] buf_out1, buf_out2;
+  wire  [31:0] buf_out1, buf_out2, buf_out3;
   input [31:0] not_in;
   wire  [31:0] not_out;
   
   and a1[31:0] (and_out, and_in1, and_in2);
   or  o1[31:0] (or_out, or_in1, or_in2);
   buf b1[31:0] (buf_out1, buf_out2, buf_in);
+  buf b2[31:0] (buf_out3, buf_in);
   not n1[31:0] (not_out, not_in);
   
   always begin
@@ -19,6 +20,7 @@ module main(and_in1, and_in2, or_in1, or_in2, buf_in, not_in);
     assert and_ok: (and_in1&and_in2)==and_out;
     assert buf_ok1: buf_in==buf_out1;
     assert buf_ok2: buf_in==buf_out2;
+    assert buf_ok3: buf_in==buf_out3;
     assert not_ok: not_in==~not_out;
   end
 
