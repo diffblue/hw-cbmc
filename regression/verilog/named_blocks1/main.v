24c8a4b0621af12cc437e50629b2d171b22b5a0c
Daniel Kroening <kroening@cs.ox.ac.uk>
2015-05-09 08:53:54 +0000

    sub-sub block

--- regression/verilog/named_blocks1/main.v
+++ regression/verilog/named_blocks1/main.v
@@ -3,13 +3,20 @@ module m;
   wire clock;
 
   always @(posedge clock) begin : my_block
+  
     reg [31:0] x;
     if(x==10)
       x=1;
     else
       x=x+1;
+      
+    begin : other_block
+      reg [31:0] y;
+      y=20;
+    end
 
     $display("my_block.x: %d", x);
+    $display("my_block.other.block.y: %d", y);
   end
   
   initial my_block.x=1;
