4f47d7e37b204eafd2ae9f0fd456f4edf9422f03
Daniel Kroening <kroening@cs.ox.ac.uk>
2015-05-22 15:57:28 +0000

    parentheses

--- regression/verilog/operators1/main.v
+++ regression/verilog/operators1/main.v
@@ -44,21 +44,21 @@ module main(input[31:0] in1, in2, in3);
   always assert logic_p8: (! 1'b0      ) == 'b1;
 
   // bit-wise
-  always assert bit_p1:  ~4'b0001           == 'b1110;
-  always assert bit_p2:  ~4'bx001           == 'bx110;
-  always assert bit_p3:  ~4'bz001           == 'bx110;
-  always assert bit_p4:  4'b0001 &  4'b1001 == 'b0001;
-  always assert bit_p5:  4'b1001 &  4'bx001 == 'bx001;
-  always assert bit_p6:  4'b1001 &  4'bz001 == 'bx001;
-  always assert bit_p7:  4'b0001 |  4'b1001 == 'b1001;
-  always assert bit_p8:  4'b0001 |  4'bx001 == 'bx001;
-  always assert bit_p9:  4'b0001 |  4'bz001 == 'bx001;
-  always assert bit_p10: 4'b0001 ^  4'b1001 == 'b1000;
-  always assert bit_p11: 4'b0001 ^  4'bx001 == 'bx000;
-  always assert bit_p12: 4'b0001 ^  4'bz001 == 'bx000;
-  always assert bit_p13: 4'b0001 ~^ 4'b1001 == 'b0111;
-  always assert bit_p14: 4'b0001 ~^ 4'bx001 == 'bx111;
-  always assert bit_p15: 4'b0001 ~^ 4'bz001 == 'bx111;                 
+  always assert bit_p1:  ~4'b0001             == 'b1110;
+  always assert bit_p2:  ~4'bx001             == 'bx110;
+  always assert bit_p3:  ~4'bz001             == 'bx110;
+  always assert bit_p4:  (4'b0001 &  4'b1001) == 'b0001;
+  always assert bit_p5:  (4'b1001 &  4'bx001) == 'bx001;
+  always assert bit_p6:  (4'b1001 &  4'bz001) == 'bx001;
+  always assert bit_p7:  (4'b0001 |  4'b1001) == 'b1001;
+  always assert bit_p8:  (4'b0001 |  4'bx001) == 'bx001;
+  always assert bit_p9:  (4'b0001 |  4'bz001) == 'bx001;
+  always assert bit_p10: (4'b0001 ^  4'b1001) == 'b1000;
+  always assert bit_p11: (4'b0001 ^  4'bx001) == 'bx000;
+  always assert bit_p12: (4'b0001 ^  4'bz001) == 'bx000;
+  always assert bit_p13: (4'b0001 ~^ 4'b1001) == 'b0111;
+  always assert bit_p14: (4'b0001 ~^ 4'bx001) == 'bx111;
+  always assert bit_p15: (4'b0001 ~^ 4'bz001) == 'bx111;
 
   // reduction
   always assert redu_p1:   & 4'b1001 == 'b0;
