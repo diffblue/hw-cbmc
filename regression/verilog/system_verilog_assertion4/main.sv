e14835d27015728fdde0276f0fd8b1cd2f4039eb
Daniel Kroening <kroening@cs.ox.ac.uk>
2015-07-18 21:28:52 +0000

    labels

--- regression/verilog/system_verilog_assertion4/main.sv
+++ regression/verilog/system_verilog_assertion4/main.sv
@@ -8,12 +8,12 @@ module main(input clk);
   always @(posedge clk) x<=x+1;
   always @(posedge clk) y<=y+2;
 
-  assert property (x==0 |-> y==0);
-  assert property (x==0 |=> y==2);
-  assert property (x==0 |-> y==0 ##1 y==2);
-  assert property (x==0 |-> y==0 ##1 y==2 ##1 y==4);
-  assert property (x==0 |-> ##1 x==1);
-  assert property (x==1 |-> ##[1:10] y==20);
-  assert property (x==0 |-> ##[1:$] y==18);
+  p1: assert property (x==0 |-> y==0);
+  p2: assert property (x==0 |=> y==2);
+  p3: assert property (x==0 |-> y==0 ##1 y==2);
+  p4: assert property (x==0 |-> y==0 ##1 y==2 ##1 y==4);
+  p5: assert property (x==0 |-> ##1 x==1);
+  p6: assert property (x==1 |-> ##[1:10] y==20);
+  p7: assert property (x==0 |-> ##[1:$] y==18);
 
 endmodule
