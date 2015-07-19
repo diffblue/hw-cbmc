3cfc3cebc3320d6426ee99ba28bc7b884f004cac
Daniel Kroening <kroening@cs.ox.ac.uk>
2015-07-19 12:24:45 +0000

    more checks

--- regression/verilog/system_verilog_assertion4/main.sv
+++ regression/verilog/system_verilog_assertion4/main.sv
@@ -8,12 +8,17 @@ module main(input clk);
   always @(posedge clk) x<=x+1;
   always @(posedge clk) y<=y+2;
 
-  p1: assert property (x==0 |-> y==0);
-  p2: assert property (x==0 |=> y==2);
-  p3: assert property (x==0 |-> y==0 ##1 y==2);
-  p4: assert property (x==0 |-> y==0 ##1 y==2 ##1 y==4);
-  p5: assert property (x==0 |-> ##1 x==1);
-  p6: assert property (x==1 |-> ##[1:10] y==20);
-  p7: assert property (x==0 |-> ##[1:$] y==18);
+  p1:  assert property (x==0 |-> y==0);
+  p2:  assert property (x==0 |-> not x==1);
+  p3:  assert property (x==0 |=> y==2);
+  p4:  assert property (x==0 |-> ##0 x==0 ##1 x==1 ##5 x==6);
+  p5:  assert property (x==0 |-> y==0 ##1 y==2);
+  p6:  assert property (x==0 |-> y==0 ##1 y==2 ##1 y==4);
+  p7:  assert property (x==0 |-> ##1 x==1);
+  p8:  assert property (x==1 |-> ##[1:10] y==20);
+  p9:  assert property (x==0 |-> ##[1:$] y==18);
+  p10: assert property (x==0 |-> ##1 x==1 and ##2 x==2);
+  p11: assert property (x==0 |-> ##1 x==1 and not ##2 x==3);
+  p12: assert property (x==0 |-> ##1 x==1 && y==2);
 
 endmodule
