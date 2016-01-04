354772cb9a3a8fe7c3bad9eebf11610341f3d15c
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-01-04 11:17:33 +0000

    added a non-inductive property

--- regression/ebmc/k-induction1/main.v
+++ regression/ebmc/k-induction1/main.v
@@ -1,14 +1,21 @@
 module main();
 
-  reg [31:0] x;
+  reg [31:0] x, y;
   wire clk;
   
   initial x=1;
+  initial y=1;
   
   always @(posedge clk)
-    if(x<1000)
+    if(x<1000) begin
       x<=x+1;
+      y<=y+1;
+    end
 
+  // true, and 1-inductive
   always assert property1: x<=1000;
+
+  // also true, but not inductive
+  always assert property2: x<=y;
 
 endmodule
