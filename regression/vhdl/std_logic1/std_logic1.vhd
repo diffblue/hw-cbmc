198d4dcee1aa3659db0a061aa42b5c40733067e1
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-02-28 20:06:35 +0000

    added report clauses

--- regression/vhdl/std_logic1/std_logic1.vhd
+++ regression/vhdl/std_logic1/std_logic1.vhd
@@ -11,17 +11,17 @@ signal a, b, c: std_logic;
 begin 
 
   process begin
-    a:='1' or   '0'; assert a='1';
-    a:='0' or   '0'; assert a='0';
-    a:='1' nor  '0'; assert a='0';
-    a:='0' nor  '0'; assert a='1';
-    a:='1' and  '0'; assert a='0';
-    a:='1' and  '1'; assert a='1';
-    a:='1' nand '0'; assert a='1';
-    a:='1' nand '1'; assert a='0';
-    a:='1' xor  '0'; assert a='1';
-    a:='1' xnor '0'; assert a='0';
-    a:=not '0';      assert a='1';
+    a:='1' or   '0'; assert a='1' report "1 or 0";
+    a:='0' or   '0'; assert a='0' report "0 or 0";
+    a:='1' nor  '0'; assert a='0' report "1 nor 0";
+    a:='0' nor  '0'; assert a='1' report "0 nor 0";
+    a:='1' and  '0'; assert a='0' report "1 and 0";
+    a:='1' and  '1'; assert a='1' report "1 and 1";
+    a:='1' nand '0'; assert a='1' report "1 nand 0";
+    a:='1' nand '1'; assert a='0' report "1 nand 1";
+    a:='1' xor  '0'; assert a='1' report "1 xor 0";
+    a:='1' xnor '0'; assert a='0' report "1 xnor 0";
+    a:=not '0';      assert a='1' report "not 0";
   end process;
 
 end OR_beh;
