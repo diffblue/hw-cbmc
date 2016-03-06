c958bc8954ed52bd0505b59ea9f1d0f55ecac3bf
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-03-06 18:01:43 +0000

    get VHDL standard packages

---
+++ src/vhdl/vhdl_std_packages.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************\
+
+Module: VHDL Standard Packages
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include "vhdl_std_packages.h"
+
+bool vhdl_std_packages(
+  symbol_tablet &,
+  message_handlert &)
+{
+  return false;
+}
---
+++ src/vhdl/vhdl_std_packages.h
@@ -0,0 +1,20 @@
+/*******************************************************************\
+
+Module: VHDL Standard Packages
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_VHDL_STD_PACKAGES_H
+#define CPROVER_VHDL_STD_PACKAGES_H
+
+#include <util/symbol_table.h>
+#include <util/message.h>
+
+bool vhdl_std_packages(
+  symbol_tablet &,
+  message_handlert &);
+
+#endif
+
