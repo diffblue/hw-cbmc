2770a9cae79dbe9f20d2a1106c301e6a92f1dc20
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-03-12 20:42:21 +0000

    libraries

---
+++ src/vhdl/vhdl_libraries.cpp
@@ -0,0 +1,53 @@
+/*******************************************************************\
+
+Module:
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include "vhdl_libraries.h"
+#include "vhdl_language.h"
+
+const char vhdl_library_standard[]=
+#include "library_standard.inc"
+;
+
+/*******************************************************************\
+
+Function: load_vhdl_library
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void load_vhdl_library(
+  const irep_idt &name,
+  symbol_tablet &symbol_table,
+  message_handlert &message_handler)
+{
+  const char *library_text;
+
+  if(name=="standard")
+  {
+    library_text=vhdl_library_standard;
+  }
+  else
+  {
+    messaget message(message_handler);
+    message.error() << "Library `" << name << "' not found"
+                    << messaget::eom;
+    return;
+  }
+
+  std::stringstream in(library_text);  
+  
+  vhdl_languaget vhdl_language;
+  vhdl_language.set_message_handler(message_handler);
+  vhdl_language.parse(in, "<builtin-library>");
+  vhdl_language.typecheck(symbol_table, "vhdl::"+id2string(name));
+}
---
+++ src/vhdl/vhdl_libraries.h
@@ -0,0 +1,15 @@
+/*******************************************************************\
+
+Module:
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include <util/symbol_table.h>
+#include <util/message.h>
+
+void load_vhdl_library(
+  const irep_idt &name,
+  symbol_tablet &,
+  message_handlert &);
