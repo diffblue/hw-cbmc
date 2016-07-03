38e53d2dda88bb4bbd905acbc33f0fc1b561b3b6
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-07-03 10:36:59 +0000

    beginnings of Verilog interpreter

---
+++ src/verilog/verilog_interpreter.cpp
@@ -0,0 +1,22 @@
+/*******************************************************************\
+
+Module: Verilog Synthesis
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include "verilog_typecheck.h"
+
+/*******************************************************************\
+
+Function: verilog_typecheckt::verilog_interpreter
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
