06d8cd0e44351c495b035b464879cc6231ed49d9
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-11-23 16:54:40 +0000

    test for function calls in continuous assignments

---
+++ regression/verilog/assignment-with-function-call/main.sv
@@ -0,0 +1,20 @@
+module top(input [7:0] a, input [7:0] b);
+
+  function [2:0] fn;
+  input [2:0] x, y;
+  begin
+    fn = x + y;
+  end
+  endfunction
+
+  wire [7:0] sum_a_b = fn(a, b),
+             sum_1_2 = fn(1, 2),
+             sum_2_3 = fn(2, 3);
+  
+  assert property (sum_a_b==a+b);
+
+  assert property (sum_1_2==3);
+
+  assert property (sum_2_3==5);
+
+endmodule
