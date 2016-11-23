3d97861bacd5d1cf013e42506621d42076544650
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-11-23 18:11:29 +0000

    test a bit more

--- regression/verilog/functioncall2/main.v
+++ regression/verilog/functioncall2/main.v
@@ -10,8 +10,10 @@ module main;
     end
   endfunction // clog2
 
-  wire [clog2(16):1] x='hff; // 4 bits
+  wire [clog2(16):1] x4='hff; // 4 bits
+  wire [clog2(17):1] x5='hff; // 5 bits
   
-  always assert test1: x=='b1111;
+  always assert test1: x4=='b1111;
+  always assert test2: x5=='b11111;
 
 endmodule
