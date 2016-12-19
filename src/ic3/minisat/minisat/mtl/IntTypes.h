ab04fc2b4da12d20c182b20e97a75549635d3fc6
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-12-19 11:26:17 +0000

    compilation fixes

--- src/ic3/minisat/minisat/mtl/IntTypes.h
+++ src/ic3/minisat/minisat/mtl/IntTypes.h
@@ -37,6 +37,10 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 
 #include <limits.h>
 
+#ifndef PRIi64
+#define PRIi64 "lld"
+#endif
+
 //=================================================================================================
 
 #endif
