975be7c8801174b1ef35fd439d977941c3e9d341
Daniel Kroening <dkr@amazon.com>
2023-10-04 10:34:15 -0700

    ebmc: add two tests for liveness properties

---
+++ regression/ebmc/SVA-LTL/lasso1.sv
@@ -0,0 +1,19 @@
+module top(input clock);
+
+  reg [31:0] counter;
+
+  initial counter = 0;
+
+  // This yields
+  // 0 1 2 3 4 5 6 7 8 9 3 4 5...
+  always @(posedge clock)
+    if(counter == 9)
+      counter = 3;
+    else
+      counter = counter + 1;
+
+  // the counter never gets to 10
+  p0: assert property (eventually counter == 10);
+
+endmodule
+
