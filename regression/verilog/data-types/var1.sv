9eb1808d94d81692b289b174ab65e865f5ee2966
Daniel Kroening <dkr@amazon.com>
2023-11-24 15:52:23 -0800

    Verilog: var declarations
    This adds System Verilog declarations with the var keyword.

---
+++ regression/verilog/data-types/var1.sv
@@ -0,0 +1,9 @@
+module main();
+
+   // IEEE 1800-2017 A.2.1.3
+   var real some_real;
+   var [31:0] some_word = 5;
+   var signed [31:0] something_signed;
+   var bit some_array[100:0];
+
+endmodule
