bfa80bf635f86a32f4548ffed9178f21ff390405
Daniel Kroening <dkr@amazon.com>
2023-12-02 10:17:43 -0800

    ebmc: force inputs to be equal when comparing states
    The definition of a counterexample with a lasso requires two states to be
    equal.  This must include the top-level inputs, or otherwise the lasso is
    not demonstrated.

---
+++ regression/verilog/system_verilog_assertion/eventually1.sv
@@ -0,0 +1,18 @@
+// count up from 0 to 10
+
+module main(input clk, input reset);
+
+  reg [3:0] counter;
+
+  initial counter = 0;
+
+  always @(posedge clk)
+    if(reset)
+      counter = 0;
+    else if(counter != 10)
+      counter = counter + 1;
+
+  // expected to fail, owing to reset
+  p0: assert property (eventually counter == 10);
+
+endmodule
