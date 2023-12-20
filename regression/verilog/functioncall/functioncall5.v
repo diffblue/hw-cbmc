90f816da43313282d58aa2a0d0a79fe96da9d174
Daniel Kroening <dkr@amazon.com>
2023-12-20 13:43:19 -0800

    Verilog: KNOWNBUG test for mixed function contexts

---
+++ regression/verilog/functioncall/functioncall5.v
@@ -0,0 +1,23 @@
+module main(input clk);
+
+  function isUpper;
+  input [7:0] in;
+  begin
+      isUpper = ~in[5];
+  end
+  endfunction
+
+  // same function used in combinational and clocked logic  
+
+  wire [7:0] my_wire1;
+  reg my_wire2;
+
+  always @my_wire1
+    my_wire2 <= isUpper(my_wire1);
+
+  reg my_reg;
+
+  always @(posedge clk)
+    my_reg = isUpper(my_wire1);
+
+endmodule
