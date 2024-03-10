d3968c79e0776fbeaf8aff6e3429714f0cd75fae
Daniel Kroening <dkr@amazon.com>
2024-03-10 16:24:32 -0700

    Verilog: extend tests for module ports
    This adds tests for input wire ports, and for ANSI-style output wire ports.

--- regression/verilog/modules/input_and_wire.v
+++ regression/verilog/modules/input_and_wire.v
@@ -1,13 +1,13 @@
 module M1(some_port);
-  output [31:0] some_port;
+  input [31:0] some_port;
   wire [31:0] some_port;
 endmodule
 
 module M2(some_port);
   // order flipped
+  input [31:0] some_port;
   wire [31:0] some_port;
-  output [31:0] some_port;
 endmodule
 
-module M3(output wire [31:0] some_port);
+module M3(input wire [31:0] some_port);
 endmodule
