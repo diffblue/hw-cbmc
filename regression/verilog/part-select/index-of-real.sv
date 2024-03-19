8b19daffd5cca365340772ae49460beef4ac655d
Daniel Kroening <dkr@amazon.com>
2024-03-19 07:40:32 -0700

    Verilog: indexed part select expressions with non-constant index
    This implements support for indexed part select expressions where the index
    is not an elaboration-time constant.

---
+++ regression/verilog/part-select/index-of-real.sv
@@ -0,0 +1,7 @@
+module main;
+
+  // 1800-2017 6.12.1
+  real some_real;
+  wire x = some_real[0];
+
+endmodule
---
+++ regression/verilog/part-select/part-select-of-real.sv
@@ -0,0 +1,7 @@
+module main;
+
+  // 1800-2017 6.12.1
+  real some_real;
+  wire [7:0] x = some_real[7:0];
+
+endmodule
