7903129e99a97ea95f2878fc920ce8363badb8d3
Daniel Kroening <dkr@amazon.com>
2024-04-27 19:12:36 -0700

    ebmc: cleanup Makefile
    This removes duplicates in the object list, and removes the check whether
    the verilog directory exists.

--- src/ebmc/ebmc_languages.cpp
+++ src/ebmc/ebmc_languages.cpp
@@ -10,9 +10,7 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <smvlang/smv_language.h>
 
-#ifdef HAVE_VERILOG
 #include <verilog/verilog_language.h>
-#endif
 
 #ifdef HAVE_VHDL
 #include <vhdl/vhdl_language.h>
@@ -38,9 +36,7 @@ void ebmc_parse_optionst::register_languages()
 {
   register_language(new_smv_language);
 
-  #ifdef HAVE_VERILOG
   register_language(new_verilog_language);
-  #endif
 
   #ifdef HAVE_VHDL
   register_language(new_vhdl_language);
