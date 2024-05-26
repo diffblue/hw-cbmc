f10e0cdf5ae8c4af0db5cfdc49bcfa111eb3775c
Daniel Kroening <dkr@amazon.com>
2024-05-26 19:52:58 +0100

    fx

--- src/nuterm/training.cpp
+++ src/nuterm/training.cpp
@@ -20,12 +20,43 @@ void ranking_function_training(
 {
   assert(net != nullptr);
 
-  for(auto &batch : data)
-    assert(batch.size(0) == 2);
+  for(auto &sample : data)
+    assert(sample.size(0) == 2);
+
+  // Form batches. These are 2 x batch-size x state-vars.
+  std::vector<std::pair<torch::Tensor, torch::Tensor>> batches;
+  std::size_t batch_size = 100;
+  std::vector<torch::Tensor> batch_current, batch_next;
+  batch_current.reserve(batch_size);
+  batch_next.reserve(batch_size);
+
+  for(auto &sample : data)
+  {
+    if(batch_current.size() == batch_size)
+    {
+      auto tensor_current = torch::stack(batch_current, 0);
+      auto tensor_next = torch::stack(batch_next, 0);
+      auto pair = std::pair<torch::Tensor, torch::Tensor>{std::move(tensor_current), std::move(tensor_next)};
+      batches.push_back(std::move(pair));
+      batch_current.clear();
+      batch_next.clear();
+    }
+    else
+    {
+      batch_current.push_back(sample[0]);
+      batch_next.push_back(sample[1]);
+    }
+  }
+
+  std::cout << "BATCHES.size(): " << batches.size() << "\n";
+  std::cout << "BATCHES[0].first.size(0): " << batches[0].first.size(0) << "\n";
+  std::cout << "BATCHES[0].second.size(0): " << batches[0].second.size(0) << "\n";
+  std::cout << "BATCHES[1].first.size(0): " << batches[1].first.size(0) << "\n";
 
   auto ranking_function_loss =
     [](const torch::Tensor &curr, const torch::Tensor &next) -> torch::Tensor {
-    assert(curr.dim() == 1 && next.dim() == 1);
+    // these are batches
+    assert(curr.dim() == 2 && next.dim() == 2);
     // The ranking needs to decrease from 'curr' to 'next'
     // by at least 'delta'. Anything less than that is loss.
     return torch::relu(next - curr + delta);
@@ -45,19 +76,20 @@ void ranking_function_training(
     size_t batch_index = 0;
 
     // Iterate the data loader to yield batches from the dataset.
-    for(auto &batch : data)
+    for(auto &batch : batches)
     {
-      //std::cout << "B: " << batch << "\n";
+      // std::cout << "B: " << batch << "\n";
 
       // Reset gradients.
       optimizer.zero_grad();
 
-      // Execute the model on the input data.
-      assert(batch.size(0) == 2);
-      torch::Tensor prediction_curr = net->forward(batch[0]);
-      torch::Tensor prediction_next = net->forward(batch[1]);
+      // Execute the model on the input data vectors.
+      torch::Tensor prediction_curr = net->forward(batch.first);
+      torch::Tensor prediction_next = net->forward(batch.second);
       // std::cout << "B: " << std::fixed << batch[0][1].item<double>() << " -> " << batch[1][1].item<double>() << "\n";
       // std::cout << "R: " << std::fixed << prediction_curr.item<double>() << " -> " << prediction_next.item<double>() << "\n";
+      std::cout << "prediction_curr: " << prediction_curr.dim() << "\n";
+      return;
 
       // Compute a loss value to judge the prediction of our model.
       torch::Tensor loss =
