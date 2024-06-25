55303bcf33f3747207ddacb51aa3317fa54c7bc5
Daniel Kroening <dkr@amazon.com>
2024-06-24 19:44:37 -0700

    SystemVerilog: test for cast from packed struct to vector

---
+++ regression/verilog/structs/structs4.sv
@@ -0,0 +1,21 @@
+module main;
+
+  // The first field is the most-significant bit.
+  struct packed {
+    bit field1, field2;
+    bit [6:0] field3;
+  } s;
+
+  initial begin
+    s.field1 = 1;
+    s.field2 = 0;
+    s.field3 = 'b1110011;
+  end
+
+  // structs can be converted without cast to bit-vectors
+  wire [8:0] w = s;
+
+  // Expected to pass.
+  p0: assert property (w == 'b1_0_1110011);
+
+endmodule
