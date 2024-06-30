0bdd66ec16e09378830fc8bd04cb6a903e95f02e
Daniel Kroening <dkr@amazon.com>
2024-06-30 12:30:58 +0100

    AIG engine: fix for new symbols for fresh inputs
    When generating the AIG, the conversion adds new symbols for the next-state
    value if none is given.  These new symbols now have a mode, and a fresh
    identifier is generated for each bit, to ensure that the type (bool) is
    consistent with the variable map.

--- src/trans-netlist/trans_to_netlist.cpp
+++ src/trans-netlist/trans_to_netlist.cpp
@@ -59,6 +59,8 @@ protected:
   netlistt &dest;
   
   literalt new_input();
+  std::size_t input_counter = 0;
+  irep_idt mode;
 
   class rhs_entryt
   {
@@ -159,7 +161,7 @@ Function: convert_trans_to_netlistt::new_input
 
 literalt convert_trans_to_netlistt::new_input()
 {
-  irep_idt id="convert::input";
+  irep_idt id = "convert::input" + std::to_string(input_counter++);
 
   if(symbol_table.symbols.find(id)==symbol_table.symbols.end())
   {
@@ -168,6 +170,7 @@ literalt convert_trans_to_netlistt::new_input()
     symbol.type=bool_typet();
     symbol.is_input=true;
     symbol.base_name="input";
+    symbol.mode = mode;
     symbol_table.add(symbol);
   }
 
@@ -286,6 +289,7 @@ void convert_trans_to_netlistt::operator()(
 
   const symbolt &module_symbol=ns.lookup(module);
   const transt &trans=to_trans_expr(module_symbol.value);
+  mode = module_symbol.mode;
 
   // build the net-list
   aig_prop_constraintt aig_prop(dest, get_message_handler());
