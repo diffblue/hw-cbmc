5a304340db37f1ce27318f7f9941c8e082008d95
Daniel Kroening <dkr@amazon.com>
2024-07-31 06:04:55 -0700

    Verilog: KNOWNBUG test for sequence implication
    For #610.

---
+++ regression/verilog/SVA/sequence_implication1.sv
@@ -0,0 +1,15 @@
+module main(input clk);
+
+  reg [31:0] counter = 0;
+
+  // 0 1 2 0 1 2 ...
+  always_ff @(posedge clk)
+    if(counter == 2)
+      counter = 0;
+    else
+      counter++;
+
+  // checks that 1 2 is followed by 0
+  assert property (@(posedge clk) counter == 1 ##1 counter == 2 |-> ##1 counter == 0);
+
+endmodule : main
