d5edadccafaa02e0886e193c43cfcd2c1b3bb09e
Daniel Kroening <dkr@amazon.com>
2024-07-31 10:36:36 -0700

    add word-level BMC encoding for SVA sequences
    This adds a BMC encoding for SVA sequences by translating an SVA sequence
    expression into a list of match point timeframe numbers and match
    conditions.

--- src/temporal-logic/temporal_logic.cpp
+++ src/temporal-logic/temporal_logic.cpp
@@ -12,15 +12,14 @@ Author: Daniel Kroening, kroening@kroening.com
 
 bool is_temporal_operator(const exprt &expr)
 {
-  return is_CTL_operator(expr) || is_LTL_operator(expr) || expr.id() == ID_A ||
-         expr.id() == ID_E || expr.id() == ID_sva_always ||
-         expr.id() == ID_sva_always || expr.id() == ID_sva_ranged_always ||
-         expr.id() == ID_sva_nexttime || expr.id() == ID_sva_s_nexttime ||
-         expr.id() == ID_sva_non_overlapped_implication ||
-         expr.id() == ID_sva_until || expr.id() == ID_sva_s_until ||
-         expr.id() == ID_sva_until_with || expr.id() == ID_sva_s_until_with ||
-         expr.id() == ID_sva_eventually || expr.id() == ID_sva_s_eventually ||
-         expr.id() == ID_sva_cycle_delay ||
+  return is_CTL_operator(expr) || is_LTL_operator(expr) ||
+         is_SVA_sequence(expr) || expr.id() == ID_A || expr.id() == ID_E ||
+         expr.id() == ID_sva_always || expr.id() == ID_sva_always ||
+         expr.id() == ID_sva_ranged_always || expr.id() == ID_sva_nexttime ||
+         expr.id() == ID_sva_s_nexttime || expr.id() == ID_sva_until ||
+         expr.id() == ID_sva_s_until || expr.id() == ID_sva_until_with ||
+         expr.id() == ID_sva_s_until_with || expr.id() == ID_sva_eventually ||
+         expr.id() == ID_sva_s_eventually || expr.id() == ID_sva_cycle_delay ||
          expr.id() == ID_sva_overlapped_followed_by ||
          expr.id() == ID_sva_nonoverlapped_followed_by;
 }
@@ -65,4 +64,16 @@ bool is_LTL(const exprt &expr)
   };
 
   return !has_subexpr(expr, non_LTL_operator);
+}
+
+bool is_SVA_sequence(const exprt &expr)
+{
+  auto id = expr.id();
+  // Note that ID_sva_overlapped_followed_by and ID_sva_nonoverlapped_followed_by
+  // are property expressions, not sequence expressions.
+  return id == ID_sva_overlapped_implication ||
+         id == ID_sva_non_overlapped_implication || id == ID_sva_cycle_delay ||
+         id == ID_sva_sequence_concatenation ||
+         id == ID_sva_sequence_intersect || id == ID_sva_sequence_first_match ||
+         id == ID_sva_sequence_throughout || id == ID_sva_sequence_within;
 }
