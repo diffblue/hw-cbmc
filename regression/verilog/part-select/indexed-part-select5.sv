4467d382f8130d1f18dbd53a223eee6892672a5e
Daniel Kroening <dkr@amazon.com>
2024-08-14 12:05:27 -0700

    Verilog: KNOWNBUG test for assignment to indexed part select
    This adds a test for an assignment to an indexed part select with constant
    indices.  The test yields invalid SMT-LIB.

---
+++ regression/verilog/part-select/indexed-part-select5.sv
@@ -0,0 +1,14 @@
+module main(input my_input);
+
+  bit [7:0] some_wire;
+
+  always @my_input begin
+    some_wire[0 +: 2] = 'b01;
+    some_wire[2 +: 2] = 'b01;
+    some_wire[4 +: 2] = 'b01;
+    some_wire[6 +: 2] = 'b01;
+  end
+
+  p0: assert final (some_wire == 'b01_01_01_01);
+
+endmodule
