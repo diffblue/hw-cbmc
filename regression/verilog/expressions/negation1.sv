88588eeabf1fc994b6d7049c7f11adaa0aa81061
Daniel Kroening <dkr@amazon.com>
2024-09-04 18:40:50 +0100

    Verilog: KNOWNBUG test for negation

---
+++ regression/verilog/expressions/negation1.sv
@@ -0,0 +1,11 @@
+module main;
+
+  property01: assert final (!0===1);
+  property02: assert final (!1===0);
+  property03: assert final (!2===0);
+  property04: assert final ($bits(!4'd1)==1);
+  property05: assert final (!1'bx===1'bx);
+  property06: assert final (!2'bxx===1'bx);
+  property07: assert final (!1'bz===1'bx);
+
+endmodule
