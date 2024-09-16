8861599829dae552720fee71d7c4854ff3db3c36
Daniel Kroening <dkr@amazon.com>
2024-09-16 12:03:56 -0700

    Verilog: synthesis for power expressions with constant rhs
    The Verilog synthesis now lowers power expressions with constant rhs to the
    equivalent multiplication.

---
+++ regression/verilog/expressions/power2.sv
@@ -0,0 +1,9 @@
+module main;
+
+  // powers with constant rhs
+  property1: assert final (3**0==1);
+  property2: assert final (3**1==3);
+  property3: assert final ((-3)**1==-3);
+  property4: assert final (3**3==27);
+
+endmodule
