94b8967b94ac2a4cffdc8af7df8af16d29dd904f
Daniel Kroening <dkr@amazon.com>
2024-10-16 17:57:14 -0700

    Update issue669.sv to use 'implies'
    This enables the test to parse.
    
    Closes #669.

--- regression/verilog/SVA/issue669.sv
+++ regression/verilog/SVA/issue669.sv
@@ -10,25 +10,19 @@ module top(input x, input y, input clk);
   assert property (not s_eventually 0);
 
   // Using until_with
-  assert property (a until_with b |-> not ((not b) s_until (not a)));
-  assert property (not ((not b) s_until (not a)) |-> a until_with b);
+  assert property ((a until_with b) implies not ((not b) s_until (not a)));
+  assert property (not ((not b) s_until (not a)) implies (a until_with b));
 
   // Until_with equivalence with until
-  assert property ((a until_with b) |-> (a until (a and b)));
-  assert property ((a until (a and b)) |-> (a until_with b));
-
-  // Property implies itself should always be true
-  assert property ((b or (always b)) |-> (b or (always b)));
-  assert property ((b or (always b)) implies (b or (always b)));
+  assert property ((a until_with b) implies (a until (a and b)));
+  assert property ((a until (a and b)) implies (a until_with b));
 
   // Definitions of strong eventually
-  assert property ((s_eventually a) |-> (1 s_until a));
-  assert property ((1 s_until a) |-> (s_eventually a));
+  assert property ((s_eventually a) implies (1 s_until a));
+  assert property ((1 s_until a) implies (s_eventually a));
 
   // Definitions of strong until
-  assert property ((a s_until b) |-> ((s_eventually b) and (a until b)));
   assert property ((a s_until b) implies ((s_eventually b) and (a until b)));
-  assert property (((s_eventually b) and (a until b)) |-> (a s_until b));
   assert property (((s_eventually b) and (a until b)) implies (a s_until b));
 
 endmodule
