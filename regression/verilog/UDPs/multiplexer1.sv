127e639d5391f8873ae3c7622a0764f7bbdc3968
Daniel Kroening <dkr@amazon.com>
2024-10-25 09:18:44 -0700

    Verilog: grammar for combinational UDPs
    This adds the 1800-2017 grammar for user-defined combinational primitives.

---
+++ regression/verilog/UDPs/multiplexer1.sv
@@ -0,0 +1,25 @@
+// 1800-2017 29.4
+primitive multiplexer (mux, control, dataA, dataB);
+   output mux;
+   input control, dataA, dataB;
+   table
+   // control  dataA  dataB   mux
+         0       1      0   :  1 ;
+         0       1      1   :  1 ;
+         0       1      x   :  1 ;
+         0       0      0   :  0 ;
+         0       0      1   :  0 ;
+         0       0      x   :  0 ;
+         1       0      1   :  1 ;
+         1       1      1   :  1 ;
+         1       x      1   :  1 ;
+         1       0      0   :  0 ;
+         1       1      0   :  0 ;
+         1       x      0   :  0 ;
+         x       0      0   :  0 ;
+         x       1      1   :  1 ;
+   endtable
+endprimitive
+
+module main;
+endmodule
