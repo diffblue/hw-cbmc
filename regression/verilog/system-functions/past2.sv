c2a3950a8926d3d468fefc9226801ae729d2fe6e
Daniel Kroening <dkr@amazon.com>
2024-11-22 20:48:56 +0000

    Instrumentation for $past
    This adds a property/model transformation for instrumenting SystemVerilog's
    $past via additional state variables into the model.  This enables the use
    of $past with the BDD, netlist-based BMC engines, and IC3.

--- regression/verilog/system-functions/past2.sv
+++ regression/verilog/system-functions/past2.sv
@@ -1,12 +1,12 @@
 module main(input clk);
 
-  reg [31:0] counter = 0;
+  reg [3:0] counter = 0;
 
   always @(posedge clk)
     if(counter < 10)
       counter++;
 
   p0: assert property (counter == 0 |-> $past(counter, 1) == 0);
-//  p1: assert property (counter != 0 && counter != 10 |-> $past(counter, 1) == counter - 1);
+  p1: assert property (counter != 0 && counter != 10 |-> $past(counter, 1) == counter - 1);
 
 endmodule
