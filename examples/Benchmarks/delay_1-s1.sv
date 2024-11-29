9692fa42b01c7570a430e06e77cfa8b4ab00b404
Michael Tautschnig <tautschn@amazon.com>
2024-11-29 16:45:16 +0100

    Safety properties

---
+++ examples/Benchmarks/delay_1-s1.sv
@@ -0,0 +1,18 @@
+module DELAY (input clk, input rst, output reg sig ,output reg err, output reg flg);
+  localparam N = 750;
+  localparam CBITS = 10;
+  reg [CBITS-1 :0] cnt;
+  always @(posedge clk) begin
+    if (rst) cnt = 0;
+    else cnt = cnt + 1;
+    if (cnt > N) begin sig = 1;
+      cnt = 0; end
+    else sig = 0;
+    if (cnt > N + 1) err = 1;
+    else err = 0;
+    if (cnt <= N) flg = 1;
+    else flg = 0;
+  end
+  s1: assert property (@(posedge clk) ((always s_eventually rst == 1) or (always s_eventually sig == 1)) and (nexttime always err == 0)) ;
+  // (F G (Verilog.DELAY.rst = FALSE) -> G F (Verilog.DELAY.sig = TRUE)) & (X G Verilog.DELAY.err = FALSE)
+endmodule
---
+++ examples/Benchmarks/delay_1-s2.sv
@@ -0,0 +1,18 @@
+module DELAY (input clk, input rst, output reg sig ,output reg err, output reg flg);
+  localparam N = 750;
+  localparam CBITS = 10;
+  reg [CBITS-1 :0] cnt;
+  always @(posedge clk) begin
+    if (rst) cnt = 0;
+    else cnt = cnt + 1;
+    if (cnt > N) begin sig = 1;
+      cnt = 0; end
+    else sig = 0;
+    if (cnt > N + 1) err = 1;
+    else err = 0;
+    if (cnt <= N) flg = 1;
+    else flg = 0;
+  end
+  s2: assert property (@(posedge clk) ((always s_eventually rst == 1) or (s_eventually always (flg == 1 s_until sig == 1)))) ;
+  // F G (Verilog.DELAY.rst = FALSE) -> F G ((Verilog.DELAY.flg = TRUE) U (Verilog.DELAY.sig = TRUE))
+endmodule
