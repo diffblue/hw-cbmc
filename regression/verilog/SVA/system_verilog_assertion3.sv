63cea6d077fe403eba7118ead6d442cff22f61dd
Daniel Kroening <dkr@amazon.com>
2024-12-19 11:17:22 -0800

    bump CBMC dependency
    The bump of the CBMC dependency fixes $onehot0.

--- regression/verilog/SVA/system_verilog_assertion3.sv
+++ regression/verilog/SVA/system_verilog_assertion3.sv
@@ -3,8 +3,9 @@ module main();
   assert final ($onehot('b0001000));
   assert final (!$onehot('b0101000));
   assert final (!$onehot('b00000));
-  assert final ($onehot0('b00000));
-  assert final ($onehot0('b000100));
-  assert final (!$onehot0('b010100));
+  assert final (!$onehot0(6'b00000));
+  assert final (!$onehot0(6'b000100));
+  assert final (!$onehot0(6'b010100));
+  assert final ($onehot0(6'b111101));
 
 endmodule
