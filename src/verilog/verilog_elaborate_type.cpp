77644f43cceb9d9d9e71bc2307cf4f74034fdcf8
Daniel Kroening <dkr@amazon.com>
2025-02-05 14:50:15 +0000

    SystemVerilog: Parsing and type checking for queue types
    This adds parsing and type checking for 1800-2017 7.10 queues.

--- src/verilog/verilog_elaborate_type.cpp
+++ src/verilog/verilog_elaborate_type.cpp
@@ -447,6 +447,13 @@ typet verilog_typecheck_exprt::elaborate_type(const typet &src)
     // package::typedef
     return elaborate_package_scope_typedef(to_type_with_subtypes(src));
   }
+  else if(src.id() == ID_verilog_queue)
+  {
+    // The subtype is the element type.
+    auto tmp = to_type_with_subtype(src);
+    tmp.subtype() = elaborate_type(tmp.subtype());
+    return std::move(tmp);
+  }
   else
   {
     throw errort().with_location(source_location)
