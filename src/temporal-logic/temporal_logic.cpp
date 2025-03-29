Romy15200/nws

SVA/LTL property instrumentation

src/temporal-logic/temporal_logic.cpp
@@ -10,6 +10,8 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <util/expr_util.h>
 
+#include "ltl.h"
+
 bool is_temporal_operator(const exprt &expr)
 {
   return is_CTL_operator(expr) || is_LTL_operator(expr) ||
@@ -63,6 +65,11 @@ bool is_LTL(const exprt &expr)
   return !has_subexpr(expr, non_LTL_operator);
 }
 
+bool is_Gp(const exprt &expr)
+{
+  return expr.id() == ID_G && !is_temporal_operator(to_G_expr(expr).op());
+}
+
 bool is_SVA_sequence(const exprt &expr)
 {
   auto id = expr.id();
@@ -104,4 +111,12 @@ bool is_SVA(const exprt &expr)
   { return is_temporal_operator(expr) && !is_SVA_operator(expr); };
 
   return !has_subexpr(expr, non_SVA_operator);
+}
+
+bool is_Buechi_SVA(const exprt &expr)
+{
+  auto unsupported_operator = [](const exprt &expr)
+  { return is_temporal_operator(expr) && !is_SVA_operator(expr); };
+
+  return !has_subexpr(expr, unsupported_operator);
 }

diffblue/hw-cbmc

src/temporal-logic/temporal_logic.cpp
@@ -10,6 +10,8 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <util/expr_util.h>
 
+#include "ltl.h"
+
 bool is_temporal_operator(const exprt &expr)
 {
   return is_CTL_operator(expr) || is_LTL_operator(expr) ||
@@ -92,6 +94,11 @@ bool is_LTL_past(const exprt &expr)
   return !has_subexpr(expr, non_LTL_past_operator);
 }
 
+bool is_Gp(const exprt &expr)
+{
+  return expr.id() == ID_G && !has_temporal_operator(to_G_expr(expr).op());
+}
+
 bool is_SVA_sequence_operator(const exprt &expr)
 {
   auto id = expr.id();
@@ -135,4 +142,12 @@ bool is_SVA(const exprt &expr)
   { return is_temporal_operator(expr) && !is_SVA_operator(expr); };
 
   return !has_subexpr(expr, non_SVA_operator);
+}
+
+bool is_Buechi_SVA(const exprt &expr)
+{
+  auto unsupported_operator = [](const exprt &expr)
+  { return is_temporal_operator(expr) && !is_SVA_operator(expr); };
+
+  return !has_subexpr(expr, unsupported_operator);
 }