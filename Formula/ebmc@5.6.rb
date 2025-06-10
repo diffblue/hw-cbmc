8451a7ba4b3ee0a0c93afc94c04472d0809ac525
Daniel Kroening <dkr@amazon.com>
2025-06-10 10:30:40 -0700

    Homebrew formulas for ebmc 5.5 and 5.6

---
+++ Formula/ebmc@5.6.rb
@@ -0,0 +1,24 @@
+class Ebmc < Formula
+  desc "Model Checker for SystemVerilog"
+  homepage "https://www.cprover.org/ebmc/"
+  url "https://github.com/diffblue/hw-cbmc.git",
+    tag: "ebmc-5.6",
+    revision: "9b402aa7cfb1157b0399f9e684bdfc1b505ed50d"
+  version "5.6"
+  license "BSD-3-Clause"
+
+  uses_from_macos "flex" => :build
+  uses_from_macos "curl" => :build
+  depends_on "bison" => :build
+
+  def install
+    system "make", "-C", "lib/cbmc/src", "minisat2-download"
+    system "make", "-C", "src"
+    system "mkdir", "-p", "#{prefix}/usr/bin"
+    system "cp", "src/ebmc/ebmc", "#{prefix}/usr/bin/"
+  end
+
+  test do
+    system "make", "-C", "regression/ebmc", "test"
+  end
+end
