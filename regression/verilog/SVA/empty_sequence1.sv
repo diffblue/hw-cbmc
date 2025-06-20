45ccadd1d1120d01789a47c69c20f080f425bd5f
Daniel Kroening <dkr@amazon.com>
2025-06-20 11:46:35 +0900

    world-level BMC: implement two empty match rules
    This implements two SVA empty match rules in the word-level BMC encoding.

--- regression/verilog/SVA/empty_sequence1.sv
+++ regression/verilog/SVA/empty_sequence1.sv
@@ -8,7 +8,12 @@ module main(input clk);
   // The empty sequence does not match
   initial p0: assert property (1[*0]);
 
-  // But can be concatenated
+  // But can be concatenated -- expected to pass
   initial p1: assert property (1[*0] ##1 x == 0);
+  initial p2: assert property (x == 0 ##1 1[*0]);
+
+  // But can be concatenated -- expected to fail
+  initial p3: assert property (1[*0] ##0 1);
+  initial p4: assert property (1 ##0 1[*0]);
 
 endmodule
