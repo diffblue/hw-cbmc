a41c4b5f61f6e39725c3cc04cd64a6782a00d7b7
Daniel Kroening <dkr@amazon.com>
2025-08-28 16:14:34 -0700

    Verilog: merge types in compound member declarations
    This merges the types in struct and union declarations.

--- regression/verilog/structs/array_in_struct1.sv
+++ regression/verilog/structs/array_in_struct1.sv
@@ -5,13 +5,13 @@ module main;
     int array1[4];
 
     // packed
-    bit [31:0] [3:0] array2;
+    bit [3:0] [31:0] array2;
   } s;
 
   initial s = '{ '{ 1, 2, 3, 4 }, '{ 1, 2, 3, 4 } };
 
   // Expected to pass.
-  p0: assert property ($bits(s) == 4);
+  p0: assert final ($bits(s) == 4 * 32 + 4 * 32);
   p11: assert property (s.array1[0] == 1);
   p12: assert property (s.array1[1] == 2);
   p13: assert property (s.array1[2] == 3);
