a35a2f425864995e9dbe0b5d183586c4cccd31e7
Daniel Kroening <dkr@amazon.com>
2025-09-14 21:35:10 -0700

    k-induction does not support $past
    k-induction requires instrumentation of $past.  This adds a removal step,
    and a precondition that $past has been removed.

--- src/ebmc/instrument_past.h
+++ src/ebmc/instrument_past.h
@@ -15,6 +15,8 @@ Author: Daniel Kroening, dkr@amazon.com
 #include "ebmc_properties.h"
 #include "transition_system.h"
 
+bool has_past(const transition_systemt &, const ebmc_propertiest &);
+
 void instrument_past(transition_systemt &, ebmc_propertiest &);
 
 #endif // EBMC_INSTRUMENT_PAST_H
