0dac84f6ddeb0c80b4725b929a3db0974983b38f
Daniel Kroening <dkr@amazon.com>
2025-11-13 13:59:25 -0800

    SystemVerilog: type parameter ports
    This adds support for SystemVerilog type parameter ports (1800-2017 6.20.3).

---
+++ regression/verilog/modules/type_parameter_port3.sv
@@ -0,0 +1,13 @@
+module sub #(type T = int)();
+
+  var T my_var;
+
+endmodule
+
+module main;
+
+  sub #(byte) submodule();
+
+  p1: assert final ($bits(submodule.my_var) == 8);
+
+endmodule // main
