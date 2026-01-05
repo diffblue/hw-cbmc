0b5ab5057827ad9add00897c6f77b531ec489750
Daniel Kroening <dkr@amazon.com>
2026-01-05 02:56:10 -0800

    SMV: smv_cases_exprt
    This adds a class for the existing SMV case ..  esac expression generated by
    the front-end.

--- src/smvlang/smv_expr.cpp
+++ src/smvlang/smv_expr.cpp
@@ -26,3 +26,23 @@ smv_extend_exprt::smv_extend_exprt(exprt __op, std::size_t __size, typet __type)
       std::move(__type)}
 {
 }
+
+cond_exprt smv_cases_exprt::lower() const
+{
+  auto &cases = this->cases();
+
+  exprt::operandst cond_ops;
+  cond_ops.reserve(cases.size() * 2);
+
+  for(auto &case_expr : cases)
+  {
+    DATA_INVARIANT(
+      case_expr.operands().size() == 2, "case expected to have two operands");
+    cond_ops.push_back(case_expr.condition());
+    cond_ops.push_back(case_expr.value());
+  }
+
+  auto cond_expr = cond_exprt{cond_ops, type()};
+  cond_expr.add_source_location() = source_location();
+  return cond_expr;
+}
