53cbdadfb7341b070f850ae497c1dbc51556e096
Daniel Kroening <dkr@amazon.com>
2026-01-20 12:57:53 -0800

    move cnf_gate_and
    The cnf_gate_and function has one user; this moves the implementation from a
    global header file to that user.

--- src/util/ebmc_util.h
+++ src/util/ebmc_util.h
@@ -25,24 +25,4 @@ inline bool to_integer_non_constant(const exprt &expr, mp_integer &int_value)
   return to_integer(to_constant_expr(expr), int_value);
 }
 
-inline void cnf_gate_and(cnft &cnf, literalt a, literalt b, literalt o) {
-  // a*b=c <==> (a + o')( b + o')(a'+b'+o)
-  bvt lits(2);
-
-  lits[0] = pos(a);
-  lits[1] = neg(o);
-  cnf.lcnf(lits);
-
-  lits[0] = pos(b);
-  lits[1] = neg(o);
-  cnf.lcnf(lits);
-
-  lits.clear();
-  lits.reserve(3);
-  lits.push_back(neg(a));
-  lits.push_back(neg(b));
-  lits.push_back(pos(o));
-  cnf.lcnf(lits);
-}
-
 #endif // HW_CBMC_UTIL_EBMC_UTIL_H
