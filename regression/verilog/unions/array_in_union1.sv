1e5ad4ee3f6b32ad7ddcf6c9501f823b6c7b9d8c
Daniel Kroening <dkr@amazon.com>
2026-03-10 17:12:17 -0700

    SystemVerilog: lowering for union member access
    Union members (e.g., structs) might need conversion; hence, add a lowering
    for the member access to unions.

--- regression/verilog/unions/array_in_union1.sv
+++ regression/verilog/unions/array_in_union1.sv
@@ -5,14 +5,16 @@ module main;
     bit [3:0] as_vector;
   } u;
 
-  initial u.array = '{ 1, 1, 0, 1 };
+  initial begin
+    u.array = '{ 1, 1, 0, 1 };
 
-  // Expected to pass.
-  p0: assert final ($bits(u) == 4);
-  p11: assert property (u.array[0] == 1);
-  p12: assert property (u.array[1] == 1);
-  p13: assert property (u.array[2] == 0);
-  p14: assert property (u.array[3] == 1);
-  p21: assert property (u.as_vector == 4'b1011);
+    // Expected to pass.
+    p0: assert($bits(u) == 4);
+    p11: assert(u.array[0] == 1);
+    p12: assert(u.array[1] == 0);
+    p13: assert(u.array[2] == 1);
+    p14: assert(u.array[3] == 1);
+    p21: assert(u.as_vector == 4'b1101);
+  end
 
 endmodule
