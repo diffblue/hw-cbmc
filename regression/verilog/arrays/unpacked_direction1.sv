99d09259816bd3e391532c478726c688397094f9
Daniel Kroening <dkr@amazon.com>
2026-03-12 10:15:17 -0700

    KNOWNBUG test for unpacked arrays

---
+++ regression/verilog/arrays/unpacked_direction1.sv
@@ -0,0 +1,16 @@
+module main;
+
+  int my_array0[5];
+  int my_array1[0:4]; // same as [5]
+  int my_array2[4:0];
+
+  initial begin
+    my_array0 = '{ 1, 2, 3, 4, 5 };
+    my_array1 = '{ 1, 2, 3, 4, 5 };
+    my_array2 = '{ 1, 2, 3, 4, 5 };
+    p0: assert(my_array0[0] == 1);
+    p1: assert(my_array1[0] == 1);
+    p2: assert(my_array2[0] == 5);
+  end
+
+endmodule
