cdb3190340133e511470b99427c8dcb1637c1faa
Daniel Kroening <dkr@amazon.com>
2026-06-08 03:18:29 -0700

    test for completeness threshold
    This adds a test for detetmining the completeness threshold of a stateless
    Verilog function.

---
+++ regression/ebmc/smv-netlist/verilog_function2.sv
@@ -0,0 +1,16 @@
+module main(input clk, input x);
+
+  // state-less
+  function [31:0] some_func(input [31:0] data_in);
+    reg [31:0] data;
+    data = data_in + 1;
+    return data;
+  endfunction
+
+  // all stateless
+  wire [31:0] out = some_func(x);
+
+  a0: assume property (@(posedge clk) x == 0);
+  p0: assert property (@(posedge clk) out == 1);
+
+endmodule
