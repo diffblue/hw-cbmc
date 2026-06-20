2d4559139648aa768e6d6d8a9c6fa8d28ee1bdd7
Daniel Kroening <dkr@amazon.com>
2026-06-20 13:33:45 -0700

    Verilog: split up let1 test
    This splits the existing let1 test into two parts.

--- regression/verilog/let/let1.sv
+++ regression/verilog/let/let1.sv
@@ -5,16 +5,10 @@ module main;
   let some_let = 8'd123;
   p0: assert final (some_let == 123 && $bits(some_let) == 8);
 
-  // The standard doesn't say, but we allow these to be elaboration-time
-  // constants.
-  parameter some_parameter = some_let;
-
-  p1: assert final (some_parameter == 123 && $bits(some_parameter) == 8);
-
   // May depend on state.
   wire some_wire;
   let some_let_eq_wire = some_wire;
 
-  p2: assert final (some_let_eq_wire == some_wire);
+  p1: assert final (some_let_eq_wire == some_wire);
 
 endmodule
