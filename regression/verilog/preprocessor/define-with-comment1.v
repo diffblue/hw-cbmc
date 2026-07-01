3b98d243c8e65170a7486bb1b796f8826f620c5d
Daniel Kroening <dkr@amazon.com>
2026-07-01 13:17:31 -0700

    KNOWNBUG tests for comment in preprocessor macro

---
+++ regression/verilog/preprocessor/define-with-comment1.v
@@ -0,0 +1,6 @@
+// The "BAR2" is part of the define
+`define FOO BAR1 \
+  // comment \
+  BAR2
+
+`FOO
---
+++ regression/verilog/preprocessor/define-with-comment2.v
@@ -0,0 +1,6 @@
+// The "BAR2" is _not_ part of the define
+`define FOO BAR1 \
+  // comment
+  BAR2
+
+`FOO
