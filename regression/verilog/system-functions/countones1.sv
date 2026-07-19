500d0aa11e41fd5fea460dab665f33fd67606cce
Daniel Kroening <dkr@amazon.com>
2026-07-19 01:59:22 -0700

    KNOWNBUG test for $countones with x/z bits

--- regression/verilog/system-functions/countones1.sv
+++ regression/verilog/system-functions/countones1.sv
@@ -1,8 +1,10 @@
 module main;
 
-  always assert ($countones('b10101)==3);
+  initial assert($countones(5'b00000)==0);
+  initial assert($countones(5'b10101)==3);
 
-  // $countones yields an elaboration-time constant
+  // $countones yields an elaboration-time constant.
+  // Icarus Verilog does not support this.
   parameter P = 123;
   parameter Q = $countones(P);
 
